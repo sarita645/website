9096528962f621ff11ae21d0cafbea6844f6004d
sarita645 <sarita445645@gmail.com>
2025-03-25 14:35:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -17,23 +17,3 @@ function copyToClipboard() {
     console.error("Failed to copy text: ", err);
   });
 }
-
-// 點擊彈窗外部區域關閉彈窗
-window.onclick = function(event) {
-  const modal = document.getElementById("myModal");
-  if (event.target == modal) {
-    closeModal();
-  }
-}
-
-// 加載導航列
-document.addEventListener("DOMContentLoaded", function () {
-  let basePath = window.location.pathname.includes('/uiux/') ? '../includes/navbar.html' : 'includes/navbar.html';
-  fetch(basePath)
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navbar-container').innerHTML = data;
-      highlightCurrentPage(); // 如果有高亮目前頁面功能，也要確保它執行
-    })
-    .catch(error => console.error('Navbar 加載錯誤:', error));
-});
